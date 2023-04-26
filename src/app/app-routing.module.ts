8d9c57534b35db47eb3609c0a8be439fab9c132e
IsabellaA <cristalvioletta1411@gmail.com>
2023-04-26 08:55:11 -0500

    primera copia

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { NoticiaComponent } from './noticia/noticia.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ContactanosComponent } from './contactanos/contactanos.component';
+
+
+const route: Routes = [
+  { path:'home', component: HomeComponent },
+  { path:'nosotros', component: NosotrosComponent },
+  { path:'noticia', component: NoticiaComponent },
+  { path:'Contactanos', component: ContactanosComponent },
+  { path :'**', redirectTo: 'home'}
+]
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(route), 
+    CommonModule
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/enviroments/enviroments.ts
@@ -0,0 +1,11 @@
+export const enviroment = {
+    production:false,
+    firebaseConfig: {
+    apiKey: "AIzaSyBYk76gybTqRzBZ9jNGrdUScmtudW_KXXA",
+    authDomain: "isabellanewscsma.firebaseapp.com",
+    projectId: "isabellanewscsma",
+    storageBucket: "isabellanewscsma.appspot.com",
+    messagingSenderId: "425502640953",
+    appId: "1:425502640953:web:eea35566d1efcebe27b6e8"
+    }
+  };
